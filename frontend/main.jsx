561aa5a3a5c37815fcc84bf33cd09a59df24f560
ingdia <d.ingabire2@alustudent.com>
2026-06-08 23:29:50 +0200

    fix: folder structure

--- frontend/main.jsx
+++
@@ -1,8 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <div className="p-4 text-2xl font-bold text-blue-600">Inventory App</div>
-  </StrictMode>
-)
